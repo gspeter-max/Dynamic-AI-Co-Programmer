fcf296a5e8a0061ce2e9e0abe13f8adf41282cc7
gspeter-max <gspeter67@gmail.com>
2025-05-13 18:26:30 +0000

    Clean repo

---
+++ code_data.py
@@ -0,0 +1,56 @@
+import os 
+import requests 
+import io 
+import zipfile 
+from dotenv import load_dotenv 
+
+
+load_dotenv('/workspaces/Dynamic-AI-Co-Programmer/dynamic_api.env')
+account_name = 'freeCodeCamp' 
+repo_name = 'freeCodeCamp' 
+target_file_name = f'downloaded/pytorch'
+hitting_url = f'https://api.github.com/repos/{account_name}/{repo_name}/zipball/main'
+api_key = os.getenv('dynamic_api') 
+
+
+headers = {
+       'Accept': 'application/vnd.github.v3+.json'                              # that is customize mime type from github so for that resone we are write 
+                                                                                # vnd --> vender ( github do that for supporting diff diff versions 
+                                                                                # git not use boring public mime type 
+        } 
+
+if api_key: 
+    headers['Authorization'] = f'token {api_key}'                               # you know headers define the http requests 
+
+response = requests.get(url = hitting_url , 
+        headers = headers , 
+        stream = True,                                                            # 1. 'stream'  for when you say to github keep the connection 
+                                                                                # 2. and first give me some basic information about that like ( sorce code etc )
+                                                                                # 3. when i am call ( response.content or response.iter_content ) then return me 
+                                                                                # whole thing( entire code ) 
+        allow_redirects = True                                                    
+    
+                                                                                # 'all_redirects = True' mean some thing that url that to you hit that say you go to next url 
+                                                                                # and nexturl say go to next like ( https://..example1 --> https://...example2) 
+                                                                                # using that that automatically handle that and reach that point that the exact data is available 
+                                                                                #( because github create temporary url for geting data 
+) 
+response.raise_for_status() 
+
+with zipfile.ZipFile(io.BytesIO(response.content)) as z :                        # Bytes --> convert raw data for file_like object ' becuase zipfile.ZipFile only aspect file_object 
+                                                                                # and inside that response.content have rew data about that
+    n_repos = list(set(repo.split('/')[0] for repo in z.namelist())) 
+    if len(n_repos) == 1: 
+        repo_name = n_repos[0] + '/' 
+
+        for repo_inside_file in z.infolist(): 
+            if not repo_inside_file.is_dir():
+                file_name = repo_inside_file.filename[len(repo_name):]
+                print(file_name) 
+                full_name = os.path.join(target_file_name,file_name) 
+
+                os.makedirs(os.path.dirname(full_name),exist_ok = True ) 
+
+                with open(full_name, 'wb') as f: 
+                    f.write(z.read(repo_inside_file.filename)) 
+
